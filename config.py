7b43049a1caab28806784cc9662386fc28ec1c33
Reedmoor <117730906+Reedmoor@users.noreply.github.com>
2025-06-04 13:51:16 +0300

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-COMPONENT_TABLES = ['motherboard', 'supply', 'cpu', 'gpu', 'cooler', 'ram', 'hdd', 'frame']
-COMPONENT_TABLES_RU = ['Материнская плата', 'Блок питания', 'Процессор', 'Видеокарта', 'Кулер', 'Оперативная память', 'Жесткий диск', 'Корпус']
-
-DB_CONFIG = {
-    'host': '127.0.0.1',
-    'user': 'postgres',
-    'password': '123456',
-    'database': 'PCBuilder'
-}
